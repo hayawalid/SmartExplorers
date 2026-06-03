6e74faa165e16e178578960786227196dcb45d40
Shahd Emad <134103174+shahdcode@users.noreply.github.com>
2026-06-04 00:36:11 +0300

    Reapply "provider complete"
    This reverts commit 404b17f1ebfa1de28930f77958a7fd716b588a48.

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -5,6 +5,7 @@ import 'screens/main_navigation_shell.dart';
 import 'screens/provider_navigation_shell.dart';
 import 'screens/admin_dashboard_screen.dart';
 import 'screens/user_profile_view_screen.dart';
+import 'screens/provider_verification_screen.dart';
 import 'theme/app_theme.dart';
 import 'theme/theme_manager.dart';
 
@@ -74,6 +75,7 @@ class _SmartExplorersAppState extends ConsumerState<SmartExplorersApp> {
         '/home': (context) => const MainNavigationShell(),
         '/provider_home': (context) => const ProviderNavigationShell(),
         '/admin': (context) => const AdminDashboardScreen(),
+        '/provider_verification': (context) => const ProviderVerificationScreen(),
       },
     );
   }
